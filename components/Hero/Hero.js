86fab5dd2c03769b01ec125ba711adbd62530538
Shubham Singh <shubhamsingh@webllisto.com>
2022-12-26 17:46:43 +0530

    Initial commit

---
+++ components/Hero/Hero.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import Button from "../../styles/GlobalComponents/Button";
+import { LeftSection } from "./HeroStyles";
+
+const Hero = (props) => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center style={{ fontSize: "5rem", lineHeight: "1.2" }}>
+        Hello, I&apos;m Shubham Joshi <br />I build things for the web
+      </SectionTitle>
+      <SectionText>
+        I&apos;m a full-stack web developer, MA specialzing in building
+        expectional,high-quality websites and applications
+      </SectionText>
+      <Button
+        onClick={() =>
+          (window.location =
+            "https://www.linkedin.com/in/shubham-joshi-005a721a4/")
+        }
+      >
+        Learn More
+      </Button>
+    </LeftSection>
+  </Section>
+);
+
+export default Hero;
